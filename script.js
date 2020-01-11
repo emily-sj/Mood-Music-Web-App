f286c751419146cc2a8cbac6d46bc3ed66e7157b
Edmond <burritotho@gmail.com>
2020-01-11 02:01:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-canvas = document.getElementById("renderer");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    ctx = canvas.getContext("2d");
- 
-    // find the center of the window
-    center_x = canvas.width / 2;
-    center_y = canvas.height / 2;
-    radius = 150;
- 
-    //draw a circle
-    ctx.beginPath();
-    ctx.arc(center_x,center_y,radius,0,2*Math.PI);
-    ctx.stroke();
- 
-    for(var i = 0; i < bars; i++){
- 
-        //divide a circle into equal parts
-        rads = Math.PI * 2 / bars;
- 
-        bar_height = 100;
-        bar_width = 2;
- 
-        x = center_x + Math.cos(rads * i) * (radius);
-	y = center_y + Math.sin(rads * i) * (radius);
-        x_end = center_x + Math.cos(rads * i)*(radius + bar_height);
-        y_end = center_y + Math.sin(rads * i)*(radius + bar_height);
- 
-        //draw a bar
-        drawBar(x, y, x_end, y_end, bar_width);
- 
-    }
-
 var canvas, ctx, center_x, center_y, radius, bars, 
 x_end, y_end, bar_height, bar_width,
 frequency_array;
@@ -40,7 +7,7 @@ function initPage(){
 audio = new Audio();
 context = new (window.AudioContext || window.webkitAudioContext)();
 analyser = context.createAnalyser();
-audio.src = "shoota.mp3"; // the source path
+audio.src = ""; // the source path
 source = context.createMediaElementSource(audio);
 source.connect(analyser);
 analyser.connect(context.destination);
